628ef246c87a6ea179884a25dbc6b38820dd5fe7
Saad Qadeer <saadqadeer1234@gmail.com>
2026-07-29 14:24:13 +0500

    day 6 : Improved PieChart Having Total Expense In the PieChart Center

--- lib/Widgets/ExpenseBreakDown_Chart.dart
+++ lib/Widgets/ExpenseBreakDown_Chart.dart
@@ -18,70 +18,93 @@ class _ExpenseBreakDownChartState extends State<ExpenseBreakDownChart> {
     return Consumer<ExpenseProvider>(
       builder: (context, provider, child) {
         // Build your chart based on the expense data
-        return SizedBox(
-          height: 250,
-          child: PieChart(
-            PieChartData(
-              sections: provider.ExpenseCatagoryTitle.map((items) {
-                return PieChartSectionData(
-                  radius: 50,
-                  color: items.toLowerCase() == "food"
-                      ? AppColors.primary
-                      : items.toLowerCase() == "shopping"
-                      ? AppColors.secondary
-                      : items.toLowerCase() == "travel"
-                      ? Colors.pinkAccent
-                      : items.toLowerCase() == "entertainment"
-                      ? Colors.grey
-                      : Colors.grey.shade300,
-                  value: items.toLowerCase() == "food"
-                      ? provider.FoodPercentage == 0.0
+        return Center(
+          child: SizedBox(
+            height: 250,
+            width: 250,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                PieChart(
+                  PieChartData(
+                    sections: provider.ExpenseCatagoryTitle.map((items) {
+                      return PieChartSectionData(
+                        radius: 60,
+                        color: items.toLowerCase() == "food"
+                            ? AppColors.primary
+                            : items.toLowerCase() == "shopping"
+                            ? AppColors.secondary
+                            : items.toLowerCase() == "travel"
+                            ? Colors.pinkAccent
+                            : items.toLowerCase() == "entertainment"
+                            ? Colors.grey
+                            : Colors.grey.shade300,
+                        value: items.toLowerCase() == "food"
+                            ? provider.FoodPercentage == 0.0
+                                  ? 0
+                                  : provider.FoodPercentage
+                            : items.toLowerCase() == "shopping"
+                            ? provider.ShoppingPercentage == 0.0
+                                  ? 0
+                                  : provider.ShoppingPercentage
+                            : items.toLowerCase() == "travel"
+                            ? provider.TravelPercentage == 0.0
+                                  ? 0
+                                  : provider.TravelPercentage
+                            : items.toLowerCase() == "entertainment"
+                            ? provider.EntertainmentPercentage == 0.0
+                                  ? 0
+                                  : provider.EntertainmentPercentage
+                            : provider.OtherPercentage == 0.0
                             ? 0
-                            : provider.FoodPercentage
-                      : items.toLowerCase() == "shopping"
-                      ? provider.ShoppingPercentage == 0.0
-                            ? 0
-                            : provider.ShoppingPercentage
-                      : items.toLowerCase() == "travel"
-                      ? provider.TravelPercentage == 0.0
-                            ? 0
-                            : provider.TravelPercentage
-                      : items.toLowerCase() == "entertainment"
-                      ? provider.EntertainmentPercentage == 0.0
-                            ? 0
-                            : provider.EntertainmentPercentage
-                      : provider.OtherPercentage == 0.0
-                      ? 0
-                      : provider.OtherPercentage,
-                  title: items.toLowerCase() == "food"
-                      ? provider.FoodPercentage.toStringAsFixed(2) == 0.00
+                            : provider.OtherPercentage,
+                        title: items.toLowerCase() == "food"
+                            ? provider.FoodPercentage.toStringAsFixed(2) == 0.00
+                                  ? null
+                                  : provider.FoodPercentage.toStringAsFixed(2)
+                            : items.toLowerCase() == "shopping"
+                            ? provider.ShoppingPercentage.toStringAsFixed(2) ==
+                                      0.00
+                                  ? null
+                                  : provider.ShoppingPercentage.toStringAsFixed(2)
+                            : items.toLowerCase() == "travel"
+                            ? provider.TravelPercentage.toStringAsFixed(2) == 0.00
+                                  ? null
+                                  : provider.TravelPercentage.toStringAsFixed(2)
+                            : items.toLowerCase() == "entertainment"
+                            ? provider.EntertainmentPercentage.toStringAsFixed(
+                                        2,
+                                      ) ==
+                                      0.00
+                                  ? null
+                                  : provider
+                                        .EntertainmentPercentage.toStringAsFixed(
+                                      2,
+                                    )
+                            : provider.OtherPercentage.toStringAsFixed(2) == 0.00
                             ? null
-                            : provider.FoodPercentage.toStringAsFixed(2)
-                      : items.toLowerCase() == "shopping"
-                      ? provider.ShoppingPercentage.toStringAsFixed(2) == 0.00
-                            ? null
-                            : provider.ShoppingPercentage.toStringAsFixed(2)
-                      : items.toLowerCase() == "travel"
-                      ? provider.TravelPercentage.toStringAsFixed(2) == 0.00
-                            ? null
-                            : provider.TravelPercentage.toStringAsFixed(2)
-                      : items.toLowerCase() == "entertainment"
-                      ? provider.EntertainmentPercentage.toStringAsFixed(2) ==
-                                0.00
-                            ? null
-                            : provider.EntertainmentPercentage.toStringAsFixed(
-                                2,
-                              )
-                      : provider.OtherPercentage.toStringAsFixed(2) == 0.00
-                      ? null
-                      : provider.OtherPercentage.toStringAsFixed(2),
-                  titleStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                            : provider.OtherPercentage.toStringAsFixed(2),
+                        titleStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    }).toList(),
                   ),
-                );
-              }).toList(),
+                ),
+
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "\$1200",
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Total Expense"),
+                  ],
+                ),
+              ],
             ),
           ),
         );
